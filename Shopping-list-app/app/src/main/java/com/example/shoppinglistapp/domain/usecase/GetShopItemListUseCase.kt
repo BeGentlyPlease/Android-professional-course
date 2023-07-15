71d215fc1d09afb684e66320b12edaea91c60ea2
Gleb Antipenko <antipenko.g.d@gmail.com>
2023-07-15 20:04:06 +0300

    fix get shop item use case name

--- Shopping-list-app/app/src/main/java/com/example/shoppinglistapp/domain/usecase/GetShopItemListUseCase.kt
+++ Shopping-list-app/app/src/main/java/com/example/shoppinglistapp/domain/usecase/GetShopItemListUseCase.kt
@@ -3,7 +3,7 @@ package com.example.shoppinglistapp.domain.usecase
 import com.example.shoppinglistapp.domain.entity.ShopItem
 import com.example.shoppinglistapp.domain.repository.ShopItemRepository
 
-class GetShopItemList(shopItemRepository: ShopItemRepository) : ShopItemUseCase(shopItemRepository) {
+class GetShopItemListUseCase(shopItemRepository: ShopItemRepository) : ShopItemUseCase(shopItemRepository) {
     fun getShopItemList(): List<ShopItem> {
         return shopItemRepository.getShopItemList()
     }
