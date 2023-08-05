e51ae58228f5ef83a7bf201ba81e82acbc90ce22
Gleb Antipenko <begentlyplease@gmail.com>
2023-08-05 21:45:47 +0300

    add fragments; add app resources

---
+++ WordComposition/app/src/main/java/com/example/word_composition/presentation/view/GameFinishedFragment.kt
@@ -0,0 +1,18 @@
+package com.example.word_composition.presentation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.word_composition.R
+
+class GameFinishedFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+    }
+}
--- WordComposition/app/src/main/java/com/example/word_composition/presentation/view/MainActivity.kt
+++ WordComposition/app/src/main/java/com/example/word_composition/presentation/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.word_composition
+package com.example.word_composition.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.word_composition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
